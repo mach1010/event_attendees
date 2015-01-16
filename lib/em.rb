523cf27ef397e46af03e94e2ec0594b58715ca36
mach1010 <jetstream1010@gmail.com>
2015-01-15 21:25:49 -0600

    refactor parsing with CSV lib

--- lib/em.rb
+++ lib/em.rb
@@ -1,10 +1,16 @@
-puts "Event Manager initialized"
+require "csv"
 
-lines = File.readlines("event_attendees.csv")
-
-lines.each_with_index do |line, index|
-  next if index === 0
-  columns = line.split(',')
-  puts columns[2]
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
-  
+
+puts "EventManager initialized."
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  puts "#{name} #{zipcode}"
+end
+
